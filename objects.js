ae88f27cf4a03efa87a368c5c3a4606f26b53af8
Alex Stolzoff <alexstolznut@hotmail.com>
2017-08-18 07:21:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -18,6 +18,6 @@ function removeFromPlaylist(playlist, artist){
   return playlist;
 }
 
-updatePlaylist(playlist, U2, Vertigo);
+updatePlaylist(playlist, "Phil Ochs", "Here's to the State of Mississippi");
 
 removeFromPlaylist(playlist, U2);
