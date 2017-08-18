5234e41971f6d4a52e82d3c7d83e42197118b897
Alex Stolzoff <alexstolznut@hotmail.com>
2017-08-18 07:17:48 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,19 @@
+var playlist = {
+  Blink-182: "First Date",
+  AFI: "Anxious",
+  The 1975: "Somebody else"
+};
+
+var obj = new Object();
+
+obj[playlist];
+
+function updatePlaylist(playlist, artist, song){
+  playlist.artist = song;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist.artist;
+  return playlist;
+}
